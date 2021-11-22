5ea17f74c0921065fa16baa9462846d83df76f39
ebotsa <87139016+ebotsa@users.noreply.github.com>
2021-11-22 20:33:18 +0100

    Update
    Update

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
-		
+	import PropTypes from 'prop-types';
+	export class MovieView extends React.Component {
+	
+	    keypressCallback(event) {
+	        console.log(event.key);
+	    }
+	
+	    componentDidMount() {
+	        document.addEventListener('keypress', this.keypressCallback);
+	    }
+	
+	    componentWillUnmount() {
+	        document.removeEventListener('keypress', this.keypressCallback);
+	    }
+	
+	    render() {
+	        const { movie, onBackClick } = this.props;
+	
 
-		export class MovieView extends React.Component {
-		
-		    render() {
-                
-                const { movie, onBackClick } = this.props;
+	        return (
+	            <div className="movie-view">
+	                <div className="movie-poster">
+	                    <img src={movie.ImagePath} />
+	                </div>
+	                <div className="movie-title">
+	                    <span className="label">Title: </span>
+	                    <span className="value">{movie.Title}</span>
+	                </div>
+	                <div className="movie-description">
+	                    <span className="label">Description: </span>
+	                    <span className="value">{movie.Description}</span>
+	                </div>
+	                <button onClick={() => { onBackClick(null); }}>Back</button>
+	
 
-		        return (
-		            <div className="movie-view">
-		                <div className="movie-poster">
-		                    <img src={movie.ImagePath} />
-		                </div>
-		                <div className="movie-title">
-		                    <span className="label">Title: </span>
-		                    <span className="value">{movie.Title}</span>
-		                </div>
-		                <div className="movie-description">
-		                    <span className="label">Description: </span>
-		                    <span className="value">{movie.Description}</span>
-		                </div>
-		             <button onClick={() => { onBackClick(null); }}>Back</button>
-
-		            </div>
-		        );
-		    }
-		}
+	            </div>
+	        );
+	    }
+	}
+	
+	MovieView.propTypes = {
+	    movie: PropTypes.shape({
+	        Title: PropTypes.string.isRequired,
+	        Description: PropTypes.string.isRequired,
+	        ImagePath: PropTypes.string.isRequired
+	    }).isRequired,
+	    onMovieClick: PropTypes.func.isRequired
+	};
 
